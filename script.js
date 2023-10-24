92ce34517098b766e7f7b7750175a5a832859016
DhirajRajguru <110520184+DhirajRajguru@users.noreply.github.com>
2023-10-24 20:10:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  let initialdiff=Number.MAX_VALUE
+let ans=-1
+for(i=0;i<=arr.length-3;i++){
+    for(j=i+1;j<=arr.length-2;j++){
+		for(k=j+1;k<=arr.length-1;k++){
+				let sum=arr[i]+arr[j]+arr[k];
+				let diff=Math.abs(sum-target);
+				if(initialdiff>diff){
+					initialdiff=diff;
+					ans=sum;
+						}
+			}
+		}
+	}
+	return ans;
 }
 
 module.exports = threeSum;
